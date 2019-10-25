f2407e8735cfc23053a26537d46a37fa50f7e365
Mario Bourgoin <mabou@microsoft.com>
2019-10-25 13:09:08 -0400

    unpin required module versions

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='StatisticsTracker',	     # This is the name of your PyPI-package.
-    version='0.1',		     # Update the version number for new releases
+    name="StatisticsTracker",	     # This is the name of your PyPI-package.
+    version="0.1.0",		     # Update the version number for new releases
+    description="Statistics capture and saving",
+    author="Daniel Grecoe",
+    author_email="grecoe@microsoft.com",
     packages=find_packages(),
-    install_requires=["azure-cli-core==2.0.63", "azure-storage-blob==1.3.1"]
+    install_requires=["azure-cli-core", "azure-storage-blob"]
 )
